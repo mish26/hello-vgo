172ed2e1bc81292acd1a9424e9bc4116eaf1a6a5
mishrun <mishrun.git@gmail.com>
2020-07-29 23:49:05 +0900

    add fmt

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+    "fmt"
     "log"
 	"net/http"
 )
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello Docker!!")
         log.Println("received request")
     })
 
